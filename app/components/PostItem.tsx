255ee858c5cd90f587d8d985ffc2691982a89e14
Mee Cha <mee.cha.nism@gmail.com>
2023-04-06 22:46:37 -0700

    Update post item template

--- app/components/PostItem.tsx
+++ app/components/PostItem.tsx
@@ -1,7 +1,6 @@
 import { getHipDate } from '@/lib/getFormattedDate';
 import Link from 'next/link';
 
-import Image from 'next/image';
 import Tags from './Tags';
 
 type Props = {
@@ -13,7 +12,7 @@ export default function PostItem({ post }: Props) {
   const formattedDate = getHipDate(date);
   return (
     <li key={id}>
-      <Link className="no-underline flex mb-2 " href={`/posts/${id}`}>
+      <Link className="no-underline flex mb-2" href={`/posts/${id}`}>
         <div className="w-full py-4">
           <p className="font-mono text-gray-400 text-sm">{formattedDate}</p>
           <h3 className="font-bold leading-8 tracking-tight m-0 p-0 text-slate-700 text-lg md:text-2xl">
